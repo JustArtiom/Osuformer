866399cf522f1eac590bbd3591ace652eb78028c
JustArtiom <artiomsiatat@gmail.com>
2026-01-19 19:30:01 +0000

    Implement diff calculator to see the parsing difference

--- scripts/parse_map.py
+++ scripts/parse_map.py
@@ -1,11 +1,17 @@
 import click
+from src.utils import read_file, diff, similarity
 from src.osu import Beatmap
 
-@click.command
-@click.argument("path", )
+@click.command()
+@click.argument("path")
 def main(path):
-  beatmap = Beatmap(file_path=path)
-  print(beatmap)
+    raw_map = read_file(path)
+    beatmap = Beatmap(raw=raw_map)
+    score = similarity(raw_map, str(beatmap))
+    
+    print(diff(raw_map, str(beatmap)))
+    print(f"Accuracy: {score:.2f}%")
+
 
 if __name__ == "__main__":
   main()
