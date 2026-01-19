74fb35c6598e24f290fbee345982be0fa8507803
JustArtiom <artiomsiatat@gmail.com>
2026-01-19 21:35:12 +0000

    Fix, atempt (?)

--- tests/test_requirements.py
+++ tests/test_requirements.py
@@ -1,8 +1,11 @@
 from scripts.requirements import requirements_up_to_date
-
+from src.utils import diff
 
 def test_requirements_txt_is_up_to_date():
-  assert requirements_up_to_date(), (
-    "requirements.txt is outdated. "
-    "Run python scripts/requirements.py and commit the updated file."
+  valid, current, generated = requirements_up_to_date()
+  assert valid, (
+    "requirements.txt is not up to date with the imports in the codebase.\n"
+    "Please run `pipreqs . --force --savepath requirements.txt` to update it.\n\n"
+    f"Diff:\n{diff(current, generated)}\n\n"
+
   )
