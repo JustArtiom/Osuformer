83f4a52adab51a08c28adc29b548da25b5c4a0d3
JustArtiom <artiomsiatat@gmail.com>
2026-01-20 03:10:11 +0000

    WIP - tokenizer unit test

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -4,7 +4,11 @@ from src.osu import (
   Difficulty,
   Circle,
   Slider,
+  SliderObjectParams,
+  SliderCurve,
+  CurveType,
   Spinner,
+  SpinnerObjectParams
 )
 from src.config import TokenizerConfig
 from src.tokenizer import Tokenizer
@@ -30,3 +34,66 @@ def test_encoding_sv_timing_point():
   readable = [tokenizer.id_to_token[t] for t in tokens]
 
   assert readable == ["SR_0", "MAP_START", "DT_0", "TP_START", "SV_4.0", "TP_END", "MAP_END", "EOS"]
+
+def test_encoding_circle_hit_object():
+  beatmap = Beatmap()
+  circle = Circle(x=256, y=192, time=1000)
+  beatmap.hit_objects.append(circle)
+
+  tokenizer = Tokenizer(config=TokenizerConfig())
+  tokens = tokenizer.encode(beatmap)
+  readable = [tokenizer.id_to_token[t] for t in tokens]
+
+  assert readable == [
+    "SR_0", "MAP_START", "DT_1000",
+    "OBJ_START", "T_CIRCLE", "X_16", "Y_12", "OBJ_END",
+    "MAP_END", "EOS"
+  ]
+
+def test_encoding_slider_hit_object():
+  beatmap = Beatmap()
+  slider = Slider(
+    x=256, 
+    y=192, 
+    time=2000, 
+    object_params=SliderObjectParams(
+      curves=[SliderCurve(curve_type=CurveType.BEZIER, curve_points=[(256,192), (300,192)])],
+    )
+  )
+  beatmap.hit_objects.append(slider)
+
+  tokenizer = Tokenizer(config=TokenizerConfig())
+  tokens = tokenizer.encode(beatmap)
+  readable = [tokenizer.id_to_token[t] for t in tokens]
+
+  assert readable == [
+    "SR_0", "MAP_START", 
+    "DT_1000", "DT_1000",
+    "OBJ_START", "T_SLIDER", 
+    "X_16", "Y_12", 
+    "SEG_BEZIER", 
+    "CP_0", "X_16", "Y_12", 
+    "CP_1", "X_19", "Y_12", 
+    "OBJ_END",
+    "MAP_END", "EOS"
+  ]
+
+def test_encode_spinner_hit_object():
+  beatmap = Beatmap()
+  spinner = Spinner(x=256, y=192, time=3810, object_params=SpinnerObjectParams(end_time=5000))
+  beatmap.hit_objects.append(spinner)
+
+  tokenizer = Tokenizer(config=TokenizerConfig())
+  tokens = tokenizer.encode(beatmap)
+  readable = [tokenizer.id_to_token[t] for t in tokens]
+
+  assert readable == [
+    "SR_0", "MAP_START", 
+    *["DT_1000"] * 3,
+    *["DT_100"] * 8,
+    "DT_10",
+    "OBJ_START", "T_SPINNER", 
+    "DT_1000", "DT_100", *["DT_10"]*9,
+    "OBJ_END",
+    "MAP_END", "EOS"
+  ]
