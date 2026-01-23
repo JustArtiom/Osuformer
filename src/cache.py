3a291ac6cb7d1c1629ccb4e0b8154149bf31f2aa
JustArtiom <artiomsiatat@gmail.com>
2026-01-23 01:47:24 +0000

    Allow workers

--- src/cache.py
+++ src/cache.py
@@ -6,9 +6,10 @@ from .config import config_options, ExperimentConfig
 @click.command()
 @click.argument("name", type=str)
 @click.option("--limit", type=int, default=-1)
+@click.option("--workers", type=int, default=None)
 @config_options
-def main(config: ExperimentConfig, name: str, limit: int):
-  dataset = Dataset(config)
+def main(config: ExperimentConfig, name: str, limit: int, workers: int):
+  dataset = Dataset(config, workers=workers)
   dataset.build_cache(name=name, limit=limit)
 
 
