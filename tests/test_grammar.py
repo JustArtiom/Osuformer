f2d19e898ee3d85258bf8ca8b3b67876c39070b2
JustArtiom <artiomsiatat@gmail.com>
2026-01-24 13:25:07 +0000

    Fix indentation - important

--- tests/test_grammar.py
+++ tests/test_grammar.py
@@ -4,21 +4,21 @@ from .test_tokenizer import test_tokens
 from src.config import ExperimentConfig
 
 def test_grammar_allowed_next_tokens():
-    config = ExperimentConfig()
-    tokenizer = Tokenizer(config.tokenizer)
-    grammar = Grammar(tokenizer)
+  config = ExperimentConfig()
+  tokenizer = Tokenizer(config.tokenizer)
+  grammar = Grammar(tokenizer)
 
-    state = GrammarState()
+  state = GrammarState()
 
-    for i, token in enumerate(test_tokens):
-      token_id = tokenizer.vocab[token]
-      allowed_tokens = grammar.allowed_next_tokens(state)
+  for i, token in enumerate(test_tokens):
+    token_id = tokenizer.vocab[token]
+    allowed_tokens = grammar.allowed_next_tokens(state)
 
-      print()
-      print(f"{test_tokens[max(0, i-10):i]}")
-      print(f"Allowed: {set(["".join(tokenizer.id_to_token[t].split('_')[0]) for t in allowed_tokens])}")
+    print()
+    print(f"{test_tokens[max(0, i-10):i]}")
+    print(f"Allowed: {set(["".join(tokenizer.id_to_token[t].split('_')[0]) for t in allowed_tokens])}")
 
-      assert tokenizer.id_to_token[token_id] in [tokenizer.id_to_token[t] for t in allowed_tokens], \
-        f"Token '{token}' not allowed in current state.\nContext {test_tokens[max(0, i-5):i+1]}"
+    assert tokenizer.id_to_token[token_id] in [tokenizer.id_to_token[t] for t in allowed_tokens], \
+      f"Token '{token}' not allowed in current state.\nContext {test_tokens[max(0, i-5):i+1]}"
 
-      state = grammar.update_state(state, token_id)
+    state = grammar.update_state(state, token_id)
